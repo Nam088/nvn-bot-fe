a1fe2db2c52b5b0c54efbbf36111db1766c338eb
Nam088 <nam077.work@gmail.com>
2025-10-25 03:11:08 +0700

    Update Next.js configuration to include assetPrefix and enable trailingSlash
    - Added assetPrefix to ensure proper loading of assets under /nvn-fe.
    - Enabled trailingSlash for consistent URL formatting in the application.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: '/nvn-fe',
+  assetPrefix: '/nvn-fe',
+  trailingSlash: true,
   images: {
     remotePatterns: [
       {
